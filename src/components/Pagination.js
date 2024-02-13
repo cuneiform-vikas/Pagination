0ed50c8a2a400ed130a7d275e605374f4010a1cc
Vikas <vikas.parmar@thecuneiform.com>
2024-02-13 16:15:24 +0530

    Setup Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,6 +7,8 @@ const Pagination = ({
   tableRow,
   setTableRow,
 }) => {
+  const tableNumbers = [...Array(TotalTables + 1).keys()].slice(1);
+
   const goToNextTable = () => {
     if (currTable < TotalTables) {
       setCurrTable(currTable + 1);
@@ -20,33 +22,66 @@ const Pagination = ({
   const goToTableByNumber = (num) => {
     setCurrTable(num);
   };
+
+  const renderPageNumbers = () => {
+    const visibleNumbers = [];
+    if (TotalTables <= 5) {
+      visibleNumbers.push(...tableNumbers);
+    } else {
+      if (currTable < 3) {
+        visibleNumbers.push(...tableNumbers.slice(0, 3));
+        visibleNumbers.push("...");
+        visibleNumbers.push(TotalTables);
+      } else if (currTable >= TotalTables - 2) {
+        // visibleNumbers.push(1, "...");
+        visibleNumbers.push(...tableNumbers.slice(tableNumbers.length - 3));
+      } else {
+        visibleNumbers.push(currTable - 1, currTable, currTable + 1);
+        visibleNumbers.push("...", TotalTables);
+      }
+    }
+
+    return visibleNumbers.map((pageNum, i) => (
+      <li
+        key={i}
+        onClick={() => {
+          if (typeof pageNum === "number") {
+            goToTableByNumber(pageNum);
+          }
+        }}
+      >
+        {pageNum}
+      </li>
+    ));
+  };
+
   return (
     <nav>
+      <div>
+        <label htmlFor="row">Rows: </label>
+        <input
+          id="row"
+          type="number"
+          value={tableRow}
+          max={6}
+          min={1}
+          onChange={(e) => setTableRow(e.target.value)}
+        />
+      </div>
+      <p>
+        {currTable} of {TotalTables}{" "}
+      </p>
+      <input
+        type="number"
+        value={currTable}
+        max={TotalTables}
+        min={1}
+        onChange={(e) => goToTableByNumber(e.target.value)}
+      />
       <ul>
-        <li>
-          Rows:{" "}
-          <input
-            type="number"
-            value={tableRow}
-            max={TotalTables}
-            min={1}
-            onChange={(e) => setTableRow(e.target.value)}
-          />
-        </li>
-        <li>
-          {currTable} of {TotalTables}{" "}
-        </li>
-        <li>
-          <input
-            type="number"
-            value={currTable}
-            max={TotalTables}
-            min={1}
-            onChange={(e) => goToTableByNumber(e.target.value)}
-          />
-        </li>
         <li onClick={() => setCurrTable(1)}>{"<<"}</li>
         <li onClick={goToPrevTable}>Prev</li>
+        {renderPageNumbers()}
         <li onClick={goToNextTable}>Next</li>
         <li onClick={() => setCurrTable(TotalTables)}>{">>"}</li>
       </ul>
